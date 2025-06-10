c5c5b5b8aa4f451736f5f82c14d67c147e40a0d1
Ereny Remon <161506319+ErenyRemon@users.noreply.github.com>
2025-06-10 09:53:24 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import '../public/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
